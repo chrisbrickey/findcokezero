b1d7197a9c08200b9db7e82ea500a6d1409659b5
chrisbrickey <cbrickey1@gmail.com>
2017-08-30 15:44:41 -0700

    draft Retailer model and migrate

--- findcoke1/app1_findcokezero/models.py
+++ findcoke1/app1_findcokezero/models.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Retailer(models.Model):
+
+    name = models.CharField(max_length=100, blank=False)
+    street_address = models.CharField(max_length=200, blank=False)
+    city = models.CharField(max_length=100, blank=False)
+    postcode = models.IntegerField()
+    country = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=40, decimal_places=20)
+    longtitude = models.DecimalField(max_digits=40, decimal_places=20)
+
+    timestamp_last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp_created = models.DateTimeField(auto_now=False, auto_now_add=True)
