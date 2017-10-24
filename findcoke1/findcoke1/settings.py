8277b9f090381e2fbf193b0cd740ef0a2895a666
chrisbrickey <cbrickey1@gmail.com>
2017-10-24 02:02:44 -0700

    move code that uses DATABASES variable below where it is defined

--- findcoke1/findcoke1/settings.py
+++ findcoke1/findcoke1/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -97,6 +95,10 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
