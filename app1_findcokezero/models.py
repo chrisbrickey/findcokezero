4062c47cc7bf58111d677fa84307aa6e6f7f091c
chrisbrickey <cbrickey1@gmail.com>
2017-11-05 23:00:35 -0800

    add uniqueness constraints to Retailer names/addresses and Soda names/abbreviations

--- app1_findcokezero/models.py
+++ app1_findcokezero/models.py
@@ -6,8 +6,8 @@ from django.db import models
 
 class Retailer(models.Model):
 
-    name = models.CharField(max_length=100, blank=False)
-    street_address = models.CharField(max_length=200, blank=False)
+    name = models.CharField(max_length=100, blank=False, unique=True)
+    street_address = models.CharField(max_length=200, blank=False, unique=True)
     city = models.CharField(max_length=100, blank=False)
     postcode = models.IntegerField(blank=True, null=True)
     country = models.CharField(max_length=100, blank=True)
@@ -17,9 +17,16 @@ class Retailer(models.Model):
     timestamp_last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp_created = models.DateTimeField(auto_now=False, auto_now_add=True)
 
+    # declares a field to display on the Django admin or anytime you want string representation of the entire object; must be unique
+    def __str__(self):
+        return self.name
 
 class Soda(models.Model):
 
-    name = models.CharField(max_length=100, blank=False)
-    abbreviation = models.CharField(max_length=2, blank=False)
+    name = models.CharField(max_length=100, blank=False, unique=True)
+    abbreviation = models.CharField(max_length=2, blank=False, unique=True)
     low_calorie = models.BooleanField(default=False)
+
+    # declares a field to display on the Django admin or anytime you want string representation of the entire object; must be unique
+    def __str__(self):
+        return self.name
