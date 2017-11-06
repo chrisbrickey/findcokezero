43f56dcb0fc4c478fe03053478bfe00626632635
chrisbrickey <cbrickey1@gmail.com>
2017-11-06 00:02:14 -0800

    allow sodas collection to be blank for a retailer

--- app1_findcokezero/models.py
+++ app1_findcokezero/models.py
@@ -27,7 +27,7 @@ class Retailer(models.Model):
     timestamp_last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp_created = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-    sodas = models.ManyToManyField(Soda)
+    sodas = models.ManyToManyField(Soda, blank=True)
 
     # declares a field to display on the Django admin or anytime you want string representation of the entire object; must be unique
     def __str__(self):
