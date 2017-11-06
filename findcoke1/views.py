1f70d67ba255744d769fd4e3609dde7b127f2844
chrisbrickey <cbrickey1@gmail.com>
2017-11-06 03:26:49 -0800

    add view for index.html template

--- findcoke1/views.py
+++ findcoke1/views.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+def index(request):
+    return render(
+        request,
+        'index.html',
+        context={},
+    )
+
 def landing_page(request):
-    html = "<html><body>Find CokeZero</body></html>"
+    html = """
+            <html>
+            <head>
+            </head>
+            <body>
+                <h1>FindCokeZero</h1>
+                </br>
+                <a href="https://www.findcokezero.com/api/">Visit the browseable API while I complete frontend development</a>
+            </body>
+            </html>
+            """
     return HttpResponse(html)
