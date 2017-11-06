f22380743b568bd70ae26d43e3145b3837db7eeb
chrisbrickey <cbrickey1@gmail.com>
2017-11-06 00:46:36 -0800

    add view that shows sodas associated with single retailer

--- app1_findcokezero/views.py
+++ app1_findcokezero/views.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
 from .models import Retailer, Soda
 from rest_framework import viewsets
 from .serializers import RetailerSerializer, SodaSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class RetailerViewSet(viewsets.ModelViewSet):
     """
@@ -20,3 +21,12 @@ class SodaViewSet(viewsets.ModelViewSet):
     """
     queryset = Soda.objects.all()
     serializer_class = SodaSerializer
+
+
+@csrf_exempt
+@api_view(['GET'])
+def retailer_sodas(request, pk):
+    retailer = Retailer.objects.filter(id=pk)[0]
+    retailer_sodas = retailer.sodas.all()
+    serializer = SodaSerializer(retailer_sodas, many=True)
+    return Response(serializer.data)
