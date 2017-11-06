4f0d6c7cfa47e17e7da953f24037d0600d0208eb
chrisbrickey <cbrickey1@gmail.com>
2017-11-06 00:01:47 -0800

    add sodas to retailer serializer

--- app1_findcokezero/serializers.py
+++ app1_findcokezero/serializers.py
@@ -4,7 +4,7 @@ from .models import Retailer, Soda
 class RetailerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Retailer
-        fields = ('id', 'name', 'street_address', 'city', 'postcode', 'country', 'latitude', 'longtitude', 'timestamp_last_updated', 'timestamp_created')
+        fields = ('id', 'name', 'street_address', 'city', 'postcode', 'country', 'latitude', 'longtitude', 'timestamp_last_updated', 'timestamp_created', 'sodas')
 
 
 class SodaSerializer(serializers.HyperlinkedModelSerializer):
