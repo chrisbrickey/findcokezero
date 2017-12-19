63824dd9ef90d5120f4784f4ebf971e3db43a018
chrisbrickey <cbrickey1@gmail.com>
2017-12-18 21:59:14 -0800

    add test for database retrieval of retailer by zipcode

--- app1_findcokezero/tests.py
+++ app1_findcokezero/tests.py
@@ -40,6 +40,41 @@ class RetailerTestCase(TestCase):
         self.assertEqual(soda.retailer_set.get(pk=retailer1.pk), retailer1)
         self.assertEqual(soda.retailer_set.get(pk=retailer2.pk), retailer2)
 
+    def test_database_retrieves_retailer_by_zip_code(self):
+        """Retailers are retreived by zipcode"""
+        retailer1 = Retailer.objects.get(street_address="598 Bryant Street")
+        retailer2 = Retailer.objects.get(street_address="820 Bush Street")
+        retailer3 = Retailer.objects.create(name="Retailer3", street_address="abc", city="San Francisco", postcode="94107")
+
+        results_94107 = Retailer.objects.filter(postcode="94107")
+        array_94107 = []
+        for retailer in results_94107:
+            array_94107.append(str(retailer.name))
+        array_94107.sort()
+
+        results_94108 = Retailer.objects.filter(postcode="94108")
+        array_94108 = []
+        for retailer in results_94108:
+            array_94108.append(str(retailer.name))
+        array_94108.sort()
+
+        self.assertEqual(len(results_94107), 2)
+        self.assertEqual(len(results_94108), 1)
+        self.assertEqual(array_94107, ['Retailer3', 'Shell'])
+        self.assertEqual(array_94108, ['Bush Market'])
+
+    # def test_database_retrieves_retailers_by_zip_code_and_soda(self):
+    #     """Retailers are retreived in a group by soda and zipcode"""
+            # retailer3 = Retailer.objects.create(name="Retailer3", street_address="abc", city="San Francisco", postcode="94107")
+            # retailer4 = Retailer.objects.create(name="Retailer4", street_address="xyz", city="San Francisco", postcode="94108")
+            # soda1 = Soda.objects.create(name="CherryCokeZero", abbreviation="CZ", low_calorie=True)
+            # soda2 = Soda.objects.create(name="Coke Classic", abbreviation="CC", low_calorie=False)
+            # retailer1.sodas.add(soda1)
+            # retailer2.sodas.add(soda1)
+            # retailer3.sodas.add(soda2)
+            # retailer4.sodas.add(soda2)
+    
+
 class RetailerWebTestCase(WebTest):
     csrf_checks = False
 
