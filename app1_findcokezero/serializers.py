83487f2c6bd0edd515ebf951ee929a411de27110
chrisbrickey <cbrickey1@gmail.com>
2017-12-24 23:35:00 -0800

    add note to restrict response data for requests with query string filters

--- app1_findcokezero/serializers.py
+++ app1_findcokezero/serializers.py
@@ -5,7 +5,7 @@ class RetailerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Retailer
         fields = ('id', 'name', 'street_address', 'city', 'postcode', 'country', 'latitude', 'longtitude', 'timestamp_last_updated', 'timestamp_created', 'sodas')
-
+        #add conditionals here to restrict amount of data sent to frontend for query string filters
 
 class SodaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
