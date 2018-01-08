fec7229a283a508e089f4d81ee39981953b728a7
chrisbrickey <cbrickey1@gmail.com>
2018-01-08 08:47:43 -0800

    refactor test for pulling sodas by retailer to only use web - not database

--- app1_findcokezero/tests/tests_sodas_web.py
+++ app1_findcokezero/tests/tests_sodas_web.py
@@ -8,16 +8,19 @@ from django.db import IntegrityError
 
 from app1_findcokezero.models import Retailer, Soda
 
-
 # web tests should not use objects created and stored in database; that is testing behavior of both the http application and the database (too much)
 class SodaWebTestCase(WebTest):
     csrf_checks = False
 
     def setUp(self):
         self.app.post_json('/api/sodas/',
-                                           params={"abbreviation": "CH", "low_calorie": "True", "name": "CherryCokeZero"})
+                           params={"abbreviation": "CH",
+                                   "low_calorie": "True",
+                                   "name": "CherryCokeZero"})
         self.app.post_json('/api/sodas/',
-                                           params={"abbreviation": "CC", "low_calorie": "True", "name": "Coke Classic"})
+                           params={"abbreviation": "CC",
+                                   "low_calorie": "False",
+                                   "name": "Coke Classic"})
 
     def test_show_sodas(self):
         # "For sodas, HTTP get request with no params retrieves all retailers"
@@ -27,21 +30,34 @@ class SodaWebTestCase(WebTest):
 
     def test_view_all_sodas_by_retailer(self):
         # "HTTP get request with retailer ID and 'sodas' in params retrieves all sodas associated with that retailer"
-        retailer = Retailer.objects.create(name="Shell", street_address="598 Bryant Street", city="San Francisco", postcode="94107")
-        soda1 = Soda.objects.create(name="Diet Coke", abbreviation="DC", low_calorie=True)
-        retailer.sodas.add(soda1)
-        soda2 = Soda.objects.get(abbreviation="CC")
-        retailer.sodas.add(soda2)
+        post_soda_response_DC = self.app.post_json('/api/sodas/',
+                                                   params={"abbreviation": "DC",
+                                                           "low_calorie": "True",
+                                                           "name": "Diet Coke"})
+        soda_url_DC = post_soda_response_DC.json["url"]
 
-        retailer_id = retailer.id
-        get_response = self.app.get("/api/retailers/%d/sodas/" % retailer_id)
 
+        post_soda_response_CF = self.app.post_json('/api/sodas/',
+                                                   params={"abbreviation": "CF",
+                                                           "low_calorie": "True",
+                                                           "name": "Caffeine Free Diet Coke"})
+        soda_url_CF = post_soda_response_CF.json["url"]
+
+
+        post_retailer_response_Shell = self.app.post_json('/api/retailers/',
+                                                          params={"city": "San Francisco",
+                                                                  "name": "Shell",
+                                                                  "postcode": "94107",
+                                                                  "street_address": "598 Bryant Street",
+                                                                  "sodas": [soda_url_DC, soda_url_CF]})
+        retailer_id_Shell = post_retailer_response_Shell.json["id"]
+
+        get_response = self.app.get("/api/retailers/%d/sodas/" % retailer_id_Shell)
         self.assertEqual(get_response.status, "200 OK")
         self.assertEqual(len(get_response.json), 2)
 
-
     def test_create_soda(self):
-        # """For sodas, HTTP request post request with valid data results in creation of object and response with all object data"""
+        # "For sodas, HTTP request post request with valid data results in creation of object and response with all object data"
         post_response = self.app.post_json('/api/sodas/',
                                            params={"abbreviation": "DC", "low_calorie": "True", "name": "Diet Coke"})
         self.assertEqual(post_response.status, "201 Created")
@@ -49,7 +65,8 @@ class SodaWebTestCase(WebTest):
         self.assertEqual(post_response.json["abbreviation"], "DC")
         self.assertEqual(post_response.json["low_calorie"], True)
         self.assertEqual(post_response.json["name"], "Diet Coke")
-        self.assertTrue(post_response.json.has_key("id"), "Expected Retailer object to have key 'id', but it was missing.")
+        self.assertTrue(post_response.json.has_key("id"),
+                        "Expected Retailer object to have key 'id', but it was missing.")
 
         new_soda_id = post_response.json["id"]
 
