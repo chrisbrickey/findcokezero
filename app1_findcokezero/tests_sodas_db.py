6e43dd85441cb111d3800f1712c2b66f201698f9
chrisbrickey <cbrickey1@gmail.com>
2018-01-07 22:36:14 -0800

    separate web and db tests into different files

---
+++ app1_findcokezero/tests_sodas_db.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# using test.TestCase instead of unittest.TestCase to make sure tests run within the suite - not just in isolation
+from django.test import TestCase
+from django_webtest import WebTest
+from django.db import IntegrityError
+
+from app1_findcokezero.models import Retailer, Soda
+
+
+class SodaDBTestCase(TestCase):
+    def setUp(self):
+        Soda.objects.create(name="CherryCokeZero", abbreviation="CZ", low_calorie=True)
+        Soda.objects.create(name="Coke Classic", abbreviation="CC", low_calorie=False)
+
+    def test_database_stores_sodas(self):
+        """Soda types are stored in database and identified by abbreviation"""
+        soda1 = Soda.objects.get(abbreviation="CZ")
+        soda2 = Soda.objects.get(abbreviation="CC")
+        self.assertEqual(soda1.name, "CherryCokeZero")
+        self.assertEqual(soda2.name, "Coke Classic")
+        self.assertEqual(soda1.low_calorie, True)
+        self.assertEqual(soda2.low_calorie, False)
+
+    def test_database_does_not_allow_duplicate_names(self):
+        """For Sodas, duplicate names are not allowed"""
+        with self.assertRaises(IntegrityError):
+            Soda.objects.create(name="Coke Classic", abbreviation="CL", low_calorie=False)
+
+    def test_database_does_not_allow_duplicate_abbreviations(self):
+        """For Sodas, duplicate abbreviations are not allowed"""
+        with self.assertRaises(IntegrityError):
+            Soda.objects.create(name="CherryCokeZero2", abbreviation="CZ", low_calorie=False)
+
+    def test_database_retrieves_soda_by_retailer(self):
+        """Sodas are retreived in a group by retailer"""
+        retailer = Retailer.objects.create(name="Shell", street_address="598 Bryant Street", city="San Francisco", postcode="94107")
+        soda1 = Soda.objects.get(abbreviation="CZ")
+        soda2 = Soda.objects.get(abbreviation="CC")
+        retailer.sodas.add(soda1, soda2)
+        self.assertEqual(retailer.sodas.get(pk=soda1.pk), soda1)
+        self.assertEqual(retailer.sodas.get(pk=soda2.pk), soda2)
