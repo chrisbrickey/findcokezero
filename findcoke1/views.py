3def8f8b8f62c217a8b80fa45bdd3a1494b3b621
Chris Brickey <cbrickey1@gmail.com>
2025-12-27 11:16:50 +0100

    Remove old, unused landing page code

--- findcoke1/views.py
+++ findcoke1/views.py
@@ -1,25 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from django.shortcuts import render
-from django.http import HttpResponse
 
+# landing page; not part of the hyper-linked api
 def index(request):
     return render(
         request,
         'index.html',
         context={},
     )
-
-def landing_page(request):
-    html = """
-            <html>
-            <head>
-            </head>
-            <body>
-                <h1>FindCokeZero</h1>
-                </br>
-                <a href="https://www.findcokezero.com/api/">Visit the browseable API while I complete frontend development</a>
-            </body>
-            </html>
-            """
-    return HttpResponse(html)
