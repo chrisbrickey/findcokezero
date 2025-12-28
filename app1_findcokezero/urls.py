89351db906d0a1188a309392c75f52105a5455bb
Chris Brickey <cbrickey1@gmail.com>
2025-12-28 11:42:42 +0100

    Update urls/routing to use modern conventions
    This app was created many years ago and recently
    updated to a more current tech stack (python3, etc).
    This PR continues that modernization.
    
    The URL patterns at the project level (findcoke1)
    were already updated. This PR updates the URL
    patterns at the app level (app1_findcokezero).

--- app1_findcokezero/urls.py
+++ app1_findcokezero/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import re_path, include
+from django.urls import path, include
 from rest_framework import routers
 from . import views
 
@@ -9,8 +9,8 @@ router.register(r'retailers', views.RetailerViewSet, basename='retailer') # adde
 router.register(r'sodas', views.SodaViewSet)
 
 urlpatterns = [
-    re_path(r'^retailers/(?P<pk>[0-9]+)/sodas/$', views.sodas_by_retailer),
-    re_path(r'^sodas/(?P<pk>[0-9]+)/retailers/$', views.retailers_by_sodas),
-    re_path(r'^', include(router.urls)),
-    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('retailers/<int:pk>/sodas/', views.sodas_by_retailer),
+    path('sodas/<int:pk>/retailers/', views.retailers_by_sodas),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
